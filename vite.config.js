84ffb245874d8d20096307eae2c2c50abd5d68d3
muhammadyunus99 <mohamedabouelsoud00@gmail.com>
2024-12-22 17:24:27 +0200

    updated the vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/second-action-react-demo/',
   test: {
     globals: true,
     environment: 'jsdom',
